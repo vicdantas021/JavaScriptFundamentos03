45ef9d47da5670b77a50b5d5c3689a86f5253977
vicdantas021 <victoria.dantas@edu.senai.br>
2026-08-11 16:38:27 -0300

    Alterando as Funcões simples

---
+++ 01-funcoes/01-funcao-simples.js
@@ -0,0 +1,17 @@
+function saudacao(){
+    console.log("Olá, turma!");
+}
+
+saudacao();
+
+function bomDia(){
+    console.log("Bom dia!")
+}
+
+bomDia()
+
+function apresentar(nome){
+    console.log(`Olá, ${nome}`)
+}
+
+apresentar("Victória");
