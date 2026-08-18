d4ddde8f705a354fe91567438b8a628ae3d58e25
vicdantas021 <victoria.dantas@edu.senai.br>
2026-08-18 09:34:20 -0300

    Adicionando informações á 01-funcoes - 02-parametros.js

---
+++ 01-funcoes/02-parametros.js
@@ -0,0 +1,18 @@
+// function apresentar(nome){
+//     console.log(`Olá, ${nome}`);
+// }
+
+// apresentar("Victória");
+
+function somar(a, b){
+    console.log(`${a} + ${b} = ${a+b}`);
+}
+
+somar(8, 15);
+
+function apresentar(nome = "Visitante"){
+    console.log(`Olá, ${nome}`);
+    }
+
+ apresentar();
+ apresentar("Victória");
