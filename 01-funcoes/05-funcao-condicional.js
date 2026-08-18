93a1a8f55d1c497a6d079db61fbfb1d5f79a558f
vicdantas021 <victoria.dantas@edu.senai.br>
2026-08-18 09:48:17 -0300

    Adicionando informações á 01-funcoes - 05-funcao-condicional.js

---
+++ 01-funcoes/05-funcao-condicional.js
@@ -0,0 +1,9 @@
+function verificandoNota(media){
+    // if(media>= 7){
+    //     return "Aprovado";
+    // }
+    // return "Reprovado"
+    return media >= 7 ? "Aprovado" : "Reprovado"
+}
+
+console.log(verificandoNota(8));
