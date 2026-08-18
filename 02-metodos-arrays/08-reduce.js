8d94fae4442b17e88d9b84151f910079f6fa89f3
vicdantas021 <victoria.dantas@edu.senai.br>
2026-08-18 14:43:38 -0300

    Criando novas pastas (03-objetos e 04-recursos-modernos) e seus elementos

--- 02-metodos-arrays/08-reduce.js
+++ 02-metodos-arrays/08-reduce.js
@@ -3,3 +3,15 @@ const valores = [10, 20, 30, 40, 50];
 const total = valores.reduce((acumulador, valor) => acumulador + valor, 0);
 
 console.log(total);
+
+// 0 + 10 = 10
+// 10 + 20 = 30
+// 30 + 30 = 60
+// 60 + 40 = 100
+// 100 + 50 = 150
+
+//Map() transorma cada item 
+//filter() filtra item
+//some() Alguem atende?
+//every() todos atendem?
+//reduce() acumula tudo em um resultado
