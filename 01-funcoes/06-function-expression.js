12b90ae7b897c7dc8e1d0d15da686579e725708c
vicdantas021 <victoria.dantas@edu.senai.br>
2026-08-18 11:33:58 -0300

    Adicionando informações á 01-funcoes - 06-function-expression.js

---
+++ 01-funcoes/06-function-expression.js
@@ -0,0 +1,10 @@
+function somarTradicional(a, b){
+    return a + b;
+}
+
+const somarAnonima = function (a,b){
+    return a+b;
+}
+
+console.log(somarTradicional(10, 5));
+console.log(somarAnonima(10, 5))
