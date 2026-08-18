2912cc0989d13cb914271ac45d80216344f9e973
vicdantas021 <victoria.dantas@edu.senai.br>
2026-08-18 13:55:22 -0300

    Adicionando informações á 02-metodos-arrays - 02-map.js

---
+++ 02-metodos-arrays/02-map.js
@@ -0,0 +1,11 @@
+const numeros = [1,2, 3, 4, 5];
+
+const dobrado =numeros.map((num)=> num * 2)
+
+console.log(dobrado)
+
+let nomes = ["Yuri", "Evellyn", "Julia", "Fernanda"]
+
+let mensagem = nomes.map((nome) => `Olá, ${nome.toUpperCase()}`);
+
+console.log(mensagem);
