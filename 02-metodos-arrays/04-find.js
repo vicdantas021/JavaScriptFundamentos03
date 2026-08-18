59083c41afd78a40b1d1fe7089fc32b07dd8069c
vicdantas021 <victoria.dantas@edu.senai.br>
2026-08-18 14:11:19 -0300

    Adicionando informações á 02-metodos-arrays - 04-find.js e 05-find-index.js

---
+++ 02-metodos-arrays/04-find.js
@@ -0,0 +1,9 @@
+const alunos = ["Sabrina", "Maria Eduarda", "Julio", "Daniel"]
+
+const aluno = alunos.find((nome) => nome === "Sabrina")
+
+console.log(aluno)
+
+const aluno1 = alunos.find((nome) => nome === "Bryan")
+
+console.log(aluno1)
---
+++ 02-metodos-arrays/05-find-index.js
@@ -0,0 +1,10 @@
+let nomes = ["Mikaele", "Emanuele", "Anna Paula", "Camila"]
+
+const indice = nomes.findIndex((n) => n === "Emanuele")
+
+console.log(indice)
+
+const indice1 = nomes.findIndex((n) => n === "Alponte")
+
+console.log(indice1)
+
