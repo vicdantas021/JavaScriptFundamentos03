162523fbff4a8dd1796827e5443b133c56d5a0b3
vicdantas021 <victoria.dantas@edu.senai.br>
2026-08-18 11:42:04 -0300

    Adicionando informações á 01-funcoes - 07-arrow-function.js

---
+++ 01-funcoes/07-arrow-function.js
@@ -0,0 +1,15 @@
+function somarTradicional(a, b){
+    return a + b;
+}
+
+const somarAnonima = function (a,b){
+    return a+b;
+}
+
+const somarArrowFuction = (a,b) => {
+    return a + b;
+}
+
+console.log(somarTradicional(10, 5))
+console.log(somarAnonima(10, 5))
+console.log(somarArrowFuction(10, 5))
